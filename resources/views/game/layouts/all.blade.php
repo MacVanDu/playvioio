215dda1272996c02f9920f3e016d9bbf4fd637d9
Dumv <du132013@gmail.com>
2026-01-09 22:46:56 +0700

    ds

--- resources/views/game/layouts/all.blade.php
+++ resources/views/game/layouts/all.blade.php
@@ -2,7 +2,7 @@
 <html lang="en" dir="ltr">
 
 <head>
-    @php($ver = '1.0.9')
+    @php($ver = '1.0.11')
     @yield('heads')
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
