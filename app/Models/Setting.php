90a1004c51f4ac65da2b61080053ed6e74110316
Dumv <du132013@gmail.com>
2026-01-09 10:53:31 +0700

    đá

--- app/Models/Setting.php
+++ app/Models/Setting.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Setting extends Model
 {
-    protected $fillable = ['key', 'value', 'note'];
+    protected $fillable = ['key', 'value', 'note','type'];
      public static function getValue(string $key, $default = null, $castInt = false)
     {
         $value = static::where('key', $key)->value('value') ?? $default;
